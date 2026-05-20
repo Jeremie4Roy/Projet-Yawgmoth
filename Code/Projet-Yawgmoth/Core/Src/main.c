32f2f817774a2efd44db744a6ae080bfb09b4f62
Jeremie4Roy <jeremie.roy10@gmail.com>
2026-05-20 09:36:30 -0400

    Code: Modification de l'IDE pour VSCode prise 3

--- Code/Projet-Yawgmoth/Core/Src/main.c
+++ Code/Projet-Yawgmoth/Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define MotorTimer = 1000;
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -40,6 +40,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
@@ -48,6 +51,8 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -65,12 +70,7 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-  unsigned char ucHealthCounter = 20;
-  unsigned char ucEnergyCounter = 0;
-  unsigned char ucStormCounter = 0;
-  unsigned char ucPoisonCounter = 0;
-  unsigned char ucMotorCounter = MotorTimer;
-  bit bMotorState = 0;
+
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -79,8 +79,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-  SPI_Init(); 
-  MAX7219_Init(); 
+
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -92,6 +91,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -100,37 +101,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    // Read the state of the buttons and update the counters accordingly
-    if (HAL_GPIO_ReadPin(HealthUpSW_GPIO_Port, HealthUpSW_Pin) == GPIO_PIN_SET) {
-      ucHealthCounter++;
-    }
-    if (HAL_GPIO_ReadPin(HealthDownSW_GPIO_Port, HealthDownSW_Pin) == GPIO_PIN_SET) {
-      ucHealthCounter--;
-    }
-    if (HAL_GPIO_ReadPin(EnergyUpSW_GPIO_Port, EnergyUpSW_Pin) == GPIO_PIN_SET) {
-      ucEnergyCounter++;
-    }
-    if (HAL_GPIO_ReadPin(EnergyDownSW_GPIO_Port, EnergyDownSW_Pin) == GPIO_PIN_SET) {
-      ucEnergyCounter--;
-    }
-    if (HAL_GPIO_ReadPin(StormUpSW_GPIO_Port, StormUpSW_Pin) == GPIO_PIN_SET) {
-      ucStormCounter++;
-    }
-    if (HAL_GPIO_ReadPin(StormRstSW_GPIO_Port, StormRstSW_Pin) == GPIO_PIN_SET) {
-      ucStormCounter = 0;
-    }
-    if ((HAL_GPIO_ReadPin(PoisonUpSW_GPIO_Port, PoisonUpSW_Pin) == GPIO_PIN_SET) && ucPoisonCounter <= 10 ) {
-      ucPoisonCounter++;
-    }
-    if (HAL_GPIO_ReadPin(PoisonRstSW_GPIO_Port, PoisonRstSW_Pin) == GPIO_PIN_SET) {
-      ucPoisonCounter = 0;
-    }
-
-    
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-
   }
   /* USER CODE END 3 */
 }
@@ -169,6 +142,76 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_SLAVE;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES_RXONLY;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
 }
 
 /**
@@ -217,29 +260,6 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void ShowHealth(unsigned char ucHealthCounter) {
-  // Display the health counter on the MAX7219 display
-  MAX7219_Display(1, ucHealthCounter % 10); // Display the units digit
-  MAX7219_Display(2, ucHealthCounter / 10); // Display the tens digit
-}
-
-void ShowEnergy(unsigned char ucEnergyCounter) {
-  // Display the energy counter on the MAX7219 display
-  MAX7219_Display(3, ucEnergyCounter % 10); // Display the units digit
-  MAX7219_Display(4, ucEnergyCounter / 10); // Display the tens digit
-}
-
-void ShowStorm(unsigned char ucStormCounter) {
-  // Display the storm counter on the MAX7219 display
-  MAX7219_Display(5, ucStormCounter % 10); // Display the units digit
-  MAX7219_Display(6, ucStormCounter / 10); // Display the tens digit
-}
-
-void ShowPoison(unsigned char ucPoisonCounter) {
-  // Display the poison counter on the MAX7219 displays
-  MAX7219_Display(7, ucPoisonCounter % 10); // Display the units digit (poison counter is only 0-9, you lose at 10 or more)
-}
-
 
 /* USER CODE END 4 */
 
