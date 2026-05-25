1787a9acfe5c94522515377a85a32d2b9706b195
Jeremie4Roy <jeremie.roy10@gmail.com>
2026-05-25 10:17:44 -0400

    Implémentation de la fonction pour lire les boutuons

--- Code/Projet-Yawgmoth/Core/Inc/Bouton.h
+++ Code/Projet-Yawgmoth/Core/Inc/Bouton.h
@@ -5,16 +5,21 @@ extern "C" {
 #include "main.h"
 #include "stm32f1xx_it.h"
 #include "stm32f1xx_hal_conf.h"
+enum eBoutonState {NOT_PRESSED, JUST_PRESSED, PRESSED, REPETITION, RELEASED};
 
     class Bouton 
     {
         private:
         GPIO_TypeDef* GPIOx;
         uint16_t GPIO_Pin;
+        unsigned char ucAntiBounceDelay;
+        unsigned char ucRepetitionDelay;
+        eBoutonState eCurrentState;
         public:
         Bouton(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-        bool isPressed();
-
+        eBoutonState GetState();
+        unsigned char ucReadAnitBounceDelay();
+        unsigned char ucReadRepetitionDelay();
     };
 }
 #endif
--- Code/Projet-Yawgmoth/Core/Src/Bouton.cpp
+++ Code/Projet-Yawgmoth/Core/Src/Bouton.cpp
@@ -1,8 +1,69 @@
 
 #include "Bouton.h"
-
+//Constructor
 Bouton::Bouton(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
     this->GPIOx = GPIOx;
     this->GPIO_Pin = GPIO_Pin;
+    ucAntiBounceDelay = 10;
+    ucRepetitionDelay = 10;
+    eCurrentState = NOT_PRESSED;
 }
+
+//Method to get the state of the button with anti-bounce and repetition delay
+eBoutonState Bouton::GetState()
+{
+    if(HAL_GPIO_ReadPin(this->GPIOx, this->GPIO_Pin) == GPIO_PIN_SET)
+    {
+        if(ucAntiBounceDelay == 0)
+        {
+            if(eCurrentState == NOT_PRESSED)
+            {
+                eCurrentState = JUST_PRESSED;
+            }else
+            {
+                eCurrentState = PRESSED;
+            }
+            if(ucRepetitionDelay == 0)
+            {
+                ucRepetitionDelay = 10;
+                eCurrentState = REPETITION;
+            }else
+            {
+                ucRepetitionDelay--;
+            }
+        }else
+        {
+            ucAntiBounceDelay--;
+        }
+    }else
+    {
+        if(ucAntiBounceDelay > 10)
+       {
+            ucRepetitionDelay = 10;
+            if(eCurrentState == REPETITION || eCurrentState == PRESSED)
+            {
+            eCurrentState = JUST_PRESSED;
+            } else
+            {
+            eCurrentState = NOT_PRESSED;
+            }
+       }else
+       {
+        ucAntiBounceDelay++;
+       }
+      
+    }
+    return eCurrentState;
+} 
+
+unsigned char Bouton::ucReadAnitBounceDelay()
+{
+    return ucAntiBounceDelay;
+}
+
+unsigned char Bouton::ucReadRepetitionDelay()
+{
+    return ucRepetitionDelay;
+}
+
