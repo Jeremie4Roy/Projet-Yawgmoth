0e7153b9c408640e2cc1d99c4c675dbdfa080044
Jeremie4Roy <jeremie.roy10@gmail.com>
2026-05-29 10:19:14 -0400

    Abandon d'utilisation de classes dans le projet

--- Code/Projet-Yawgmoth/Core/Inc/Bouton.h
+++ Code/Projet-Yawgmoth/Core/Inc/Bouton.h
@@ -1,26 +1,9 @@
 #ifndef BOUTON_H
 #define BOUTON_H
-#ifdef __cplusplus
-extern "C" {
 #include "main.h"
 #include "stm32f1xx_it.h"
 #include "stm32f1xx_hal_conf.h"
 enum eBoutonState {NOT_PRESSED, JUST_PRESSED, PRESSED, REPETITION, RELEASED};
 
-    class Bouton 
-    {
-        private:
-        GPIO_TypeDef* GPIOx;
-        uint16_t GPIO_Pin;
-        unsigned char ucAntiBounceDelay;
-        unsigned char ucRepetitionDelay;
-        eBoutonState eCurrentState;
-        public:
-        Bouton(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
-        eBoutonState GetState();
-        unsigned char ucReadAnitBounceDelay();
-        unsigned char ucReadRepetitionDelay();
-    };
-}
-#endif
+ 
 #endif
